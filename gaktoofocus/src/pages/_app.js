37904ca7cb248813eb72e2abd93e4475a7c31c77
Mis311 <55878506+Mis311@users.noreply.github.com>
2023-03-24 20:36:33 +0900

    sorting junks

--- gaktoofocus/src/pages/_app.js
+++ gaktoofocus/src/pages/_app.js
@@ -1,9 +1,8 @@
-<<<<<<< HEAD
+
 import "@/styles/globals.css";
-=======
 import '@/styles/globals.css'
 
->>>>>>> 99d90aefa8ffbd5b12298bc015aa8fee644302f5
+
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />;
 }
