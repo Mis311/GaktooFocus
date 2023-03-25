da18d313ca5ba32a769ad9cbefc478f868ca1e2a
Mis311 <55878506+Mis311@users.noreply.github.com>
2023-03-26 01:22:45 +0900

    Todo styling

--- gaktoofocus/components/to-do-list/ToDoList.jsx
+++ gaktoofocus/components/to-do-list/ToDoList.jsx
@@ -45,26 +45,26 @@ function TodoList() {
   
   return (
     <div className="container w-120 my-8 flex">
+    
       <h1 className="text-3xl font-semibold mb-4">To-Do List</h1>
-      <div className="my-4">
-        <input className="border-2 rounded-lg px-4 py-2 mr-4" type="text" placeholder="Enter note title" value={newNoteTitle} onChange={handleTitleChange} />
-        <select className="border-2 rounded-lg px-4 py-2" value={newNoteCategory} onChange={handleCategoryChange}>
-          <option value="Health">Health</option>
-          <option value="Project">Project</option>
-          <option value="Programming">Programming</option>
-          <option value="Others">Others</option>
-        </select>
-        <button className="bg-blue-500 text-white px-4 py-2 rounded-lg ml-4" onClick={handleAddNote}>Add Note</button>
-      </div>
-      
+        <div className="my-4">
+          <input className="border-2 rounded-lg px-4 py-2 mr-4" type="text" placeholder="Enter note title" value={newNoteTitle} onChange={handleTitleChange} />
+          <select className="border-2 rounded-lg px-4 py-2" value={newNoteCategory} onChange={handleCategoryChange}>
+            <option value="Health">Health</option>
+            <option value="Project">Project</option>
+            <option value="Programming">Programming</option>
+            <option value="Others">Others</option>
+          </select>
+          <button className="bg-blue-500 text-white px-4 py-2 rounded-lg ml-4" onClick={handleAddNote}>Add Note</button>
+        </div>
+        <div className='flex-col'>
       <div className="my-4">
         <h2 className="text-xl font-semibold mb-2">Today's To-Do List</h2>
-          <BackgroundSelector />
-          <SessionTimer />
+   
         {todayNotes.length === 0 && <p>No notes for today yet.</p>}
         {todayNotes.map(note => (
-          <div className={`border-2 p-2 mb-2 ${note.category === "Health" ? "border-red-500" : note.category === "Project" ? "border-purple-500" : note.category === "Programming" ? "border-green-500" : "border-gray-500"}`}>
-            <div className="flex justify-between items-center mb-2">
+          <div className={`flex rounded-md border-2 p-2 mb-2 ${note.category === "Health" ? "border-red-500" : note.category === "Project" ? "border-purple-500" : note.category === "Programming" ? "border-green-500" : "border-gray-500"}`}>
+            <div className="flex-left  justify-between items-center mb-2">
               <div className="my-2">{note.title}</div>
               <button className="bg-blue-500 text-white px-4 py-2 rounded-lg" onClick={() => setNotes([...notes, note])}>Move back to All</button>
             </div>
@@ -72,12 +72,15 @@ function TodoList() {
           </div>
         ))}
       </div>
-      
+        
+          <BackgroundSelector />
+          <SessionTimer />
+        </div>
       <div className="my-4">
         <h2 className="text-xl font-semibold mb-2">All To-Do List</h2>
         {notes.length === 0 && <p>No notes to display.</p>}
         {notes.map(note => (
-          <div className={`border-2 p-2 mb-2 ${note.category === "Health" ? "border-red-500" : note.category === "Project" ? "border-purple-500" : note.category === "Programming" ? "border-green-500" : "border-gray-500"}`}>
+          <div className={`border-2 rounded-md p-2 mb-2 ${note.category === "Health" ? "border-red-500" : note.category === "Project" ? "border-purple-500" : note.category === "Programming" ? "border-green-500" : "border-gray-500"}`}>
             <div className="flex justify-between items-center mb-2">
               <div className="my-2">{note.title}</div>
               <button className="bg-blue-500 text-white px-4 py-2 rounded-lg" onClick={() => handleMoveToToday(note.id)}>Move to Today</button>
@@ -85,6 +88,7 @@ function TodoList() {
             <div className="text-sm font-medium">{note.category}</div>
           </div>
         ))}
+        
       </div>
     </div>
   );
